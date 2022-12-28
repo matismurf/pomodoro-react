7509931af71dde85c0caf8637978f0660bc40573
Matias K76 <55673958+matismurf@users.noreply.github.com>
2022-12-28 19:33:38 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+import {ThemeProvider} from 'styled-components'
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
+import { Router } from './Router'
+import {BrowserRouter} from 'react-router-dom'
+import { createContext } from 'react'
+import { CyclesContextProvider } from './contexts/CyclesContext'
+
+
+
+export function App() {
+  
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
+      <GlobalStyle/>
+    </ThemeProvider>
+  )
+}
+
+export default App
